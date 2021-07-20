12287261a6ebfd565a416ec3dac8d99dedf3e1d6
rgplantz <rgplantz@outlook.com>
2021-07-20 15:46:34 -0700

    chapter 9 listing

---
+++ chapter_09/Text_Listings/inches2feet/inches2feet.c
@@ -0,0 +1,25 @@
+/* inches2feet.c
+ * Converts inches to feet and inches.
+ */
+
+#include <stdio.h>
+#define inchesPerFoot 12
+
+int main(void)
+{
+  register int feet;
+  register int inchesRem;
+  int inches;
+  int *ptr;
+
+  ptr = &inches;
+
+  printf("Enter inches: ");
+  scanf("%i", ptr);
+  
+  feet = inches / inchesPerFoot;
+  inchesRem = inches % inchesPerFoot;
+  printf("%i\" = %i' %i\"\n", inches, feet, inchesRem);
+
+  return 0;
+}
