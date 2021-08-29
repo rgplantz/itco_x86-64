52dfc67ceb97bac81885738fd61946cf24c7e0ab
rgplantz <rgplantz@outlook.com>
2021-08-28 17:32:37 -0700

    build broken

---
+++ build/chapter_13/Text_Listings/helloWorld-do_C/helloWorld-do.s
@@ -0,0 +1,32 @@
+        .file   "helloWorld-do.c"
+        .intel_syntax noprefix
+        .text
+        .section        .rodata
+.LC0:
+        .string "Hello, World!\n"
+        .text
+        .globl  main
+        .type   main, @function
+main:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        lea     rax, .LC0[rip]
+        mov     QWORD PTR -8[rbp], rax
+.L2:
+        mov     rax, QWORD PTR -8[rbp]
+        mov     edx, 1
+        mov     rsi, rax
+        mov     edi, 1
+        call    write@PLT
+        add     QWORD PTR -8[rbp], 1
+        mov     rax, QWORD PTR -8[rbp]
+        movzx   eax, BYTE PTR [rax]
+        test    al, al
+        jne     .L2
+        mov     eax, 0
+        leave
+        ret
+        .size   main, .-main
+        .ident  "GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+        .section        .note.GNU-stack,"",@progbits
---
+++ build/chapter_13/Text_Listings/helloWorld-while_C/helloWorld-bp.s
@@ -0,0 +1,34 @@
+        .file   "helloWorld.c"
+        .intel_syntax noprefix
+        .text
+        .section        .rodata
+.LC0:
+        .string "Hello, World!\n"
+        .text
+        .globl  main
+        .type   main, @function
+main:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        lea     rax, .LC0[rip]          ## address of text string
+        mov     QWORD PTR -8[rbp], rax
+        jmp     .L2                     ## jump to bottom
+.L3:
+        mov     rax, QWORD PTR -8[rbp]  ## address of current character
+        mov     edx, 1                  ## one character
+        mov     rsi, rax                ## pass address
+        mov     edi, 1                  ## STDOUT_FILENO
+        call    write@PLT
+        add     QWORD PTR -8[rbp], 1    ## increment pointer
+.L2:
+        mov     rax, QWORD PTR -8[rbp]  ## address of current character
+        movzx   eax, BYTE PTR [rax]     ## load character
+        test    al, al                  ## NUL character?
+        jne     .L3                     ## no, continue looping
+        mov     eax, 0                  ## yes, all done
+        leave
+        ret
+        .size   main, .-main
+        .ident  "GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+        .section        .note.GNU-stack,"",@progbits
