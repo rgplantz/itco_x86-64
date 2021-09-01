32ee78be85e506caad5a6312c6ebd8ad61b6bfa3
rgplantz <rgplantz@outlook.com>
2021-08-31 20:50:20 -0700

    Add two exercises.

---
+++ chapter_16/Your_Turn/changeCase_asm/change.s
@@ -0,0 +1,42 @@
+# change.s
+# Change alphabetic characters in a C string to opposite case.
+# Calling sequence:
+#   rdi <- pointer to source string
+#   rsi <- pointer to destination string
+#   returns number of characters processed.
+        .intel_syntax noprefix
+
+# Stack frame
+        .equ    count,-4
+        .equ    localSize,-16
+# Useful constants
+        .equ    changeMask,0x20
+        .equ    NUL,0
+# Code
+        .text
+        .globl  change
+        .type   change, @function
+change:
+        push    rbp         # save frame pointer
+        mov     rbp, rsp    # set new frame pointer
+        add     rsp, localSize      # for local var.
+        
+        mov     dword ptr count[rbp], 0
+whileLoop:
+        mov 	  al, byte ptr [rdi]  # char from source
+        mov     byte ptr [rsi], al  # char to destination
+        cmp     al, NUL             # was it the end?
+        je      allDone             # yes, all done
+        xor     byte ptr [rsi], changeMask # no, make sure it's lower
+        inc     rdi         # increment
+        inc     rsi         #      pointers
+        inc     dword ptr count[rbp]    # and counter
+        jmp     whileLoop   # continue loop
+allDone:
+        mov     byte ptr [rsi], al  # finish with NUL
+        mov     eax, dword ptr count[rbp]   # return count
+
+        mov     rsp, rbp    # restore stack pointer
+        pop     rbp         # and caller frame pointer
+        ret
+
