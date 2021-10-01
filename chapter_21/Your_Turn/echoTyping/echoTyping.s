b0f743f7ca1dbf0fd8efb425fef6ac720fef52db
rplantz <plantz@ieee.org>
2021-10-01 14:00:25 -0700

    Update Chapter 21 Your Turn exercise

--- chapter_21/Your_Turn/echoTyping/echoTyping.s
+++ chapter_21/Your_Turn/echoTyping/echoTyping.s
@@ -11,17 +11,13 @@
         .equ    WRITE,1
         .equ    EXIT,60
 # Stack frame
-        .equ    theText,-128
-        .equ    localSize,-128
+        .equ    aChar,-16
+        .equ    localSize,-16
 # Data
         .section    .rodata
-msg1:
+msg:
         .ascii  "Enter up to 127 characters and press enter:\n"
-        .equ    msg1size, .-msg1
-msg2:
-        .ascii  "You entered:\n"
-        .equ    msg2size, .-msg2
-        
+        .equ    msgsize, .-msg
 # Code
         .text               # text segment
         .globl  myStart
@@ -31,41 +27,30 @@ myStart:
         mov     rbp, rsp    # set new frame pointer
         add     rsp, localSize  # for local var.
 
-        mov     rdx, msg1size   # prompt user
-        lea     rsi, msg1[rip]
+        mov     rdx, msgsize    # prompt user
+        lea     rsi, msg[rip]
         mov     rdi, STDOUT
         mov     eax, WRITE
         syscall
         
-        lea     rbx, theText[rbp]   # place to store input
-readLoop:
+        lea     rbx, aChar[rbp] # place to store input
+rwLoop:
         mov     rdx, 1      # one character
         mov     rsi, rbx
         mov     rdi, STDIN
         mov     eax, READ
         syscall             # request kernel service
-        cmp     byte ptr [rbx], enter   # enter key?
-        je      show        # yes, show user
-        inc     rbx         # no, increment pointer
-        jmp     readLoop    # and continue
-show:
-        mov     rdx, msg2size   # show user
-        lea     rsi, msg2[rip]
-        mov     rdi, STDOUT
-        mov     eax, WRITE
-        syscall
 
-        lea     rbx, theText[rbp]   # place input stored
-writeLoop:
         mov     rdx, 1      # one character
         mov     rsi, rbx
         mov     rdi, STDOUT
         mov     eax, WRITE
         syscall             # request kernel service
+
         cmp     byte ptr [rbx], enter   # enter key?
         je      allDone     # yes, all done
         inc     rbx         # no, increment pointer
-        jmp     writeLoop   # and continue
+        jmp     rwLoop      # and continue
 allDone:
         mov     rsp, rbp    # yes, restore stack pointer
         pop     rbp         # and caller frame pointer
